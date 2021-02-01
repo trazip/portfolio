a9c544fe47b116064ec17381b2e26867aaf95174
Pierre Penhard <p.penhard@gmail.com>
2021-02-01 23:12:11 +0100

    Work in Progress

--- app.js
+++ app.js
@@ -4,3 +4,38 @@ ScrollReveal().reveal('.headline', {
   easing: 'ease-out',
   distance: '50px'
 });
+
+const memojis = document.getElementById("memojis")
+
+setTimeout(function(){
+  if (memojis) {
+    const firstItem = document.getElementById("memoji-1");
+    const secondItem = document.getElementById("memoji-2");
+    firstItem.classList.toggle('hidden');
+    secondItem.classList.toggle('hidden');
+  }
+}, 1000);
+
+setTimeout(function(){
+  if (memojis) {
+    const firstItem = document.getElementById("memoji-1");
+    const secondItem = document.getElementById("memoji-2");
+    firstItem.classList.toggle('hidden');
+    secondItem.classList.toggle('hidden');
+  }
+}, 1200);
+
+if (memojis) {
+  const firstItem = document.getElementById("memoji-1");
+  const secondItem = document.getElementById("memoji-2");
+
+  memojis.addEventListener('mouseenter', () => {
+    firstItem.classList.toggle('hidden');
+    secondItem.classList.toggle('hidden');
+  })
+
+  memojis.addEventListener('mouseleave', () => {
+    firstItem.classList.toggle('hidden');
+    secondItem.classList.toggle('hidden');
+  })
+}
